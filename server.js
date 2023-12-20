73c07030876a17546f534770c99f70d70238625b
Bokang Keith Mogomotsi <bigbk75@gmail.com>
2023-12-20 01:11:58 -0500

    Added user creation

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const PORT = 8080;
 const express = require("express");
 const fs = require("fs");
 const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+
 const app = express();
+
+//TESTING
+const users = [];
 
 //Parse the body as json everytime we receive a request
 app.use(bodyParser.json());
@@ -12,6 +17,36 @@ app.get("/", (req, res) => {
   res.status(200).send({ message: "Hey there ;)" });
 });
 
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+app.post("/users", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log(salt);
+    const user = { username, password: hashedPassword };
+    users.push(user);
+    res.status(201).send("Added User");
+  } catch {
+    res.status(500).send();
+  }
+});
+
+app.post("/users/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find((user) => user.username == username);
+  if (user == null) {
+    return res.status(400).send("Cannot find user");
+  }
+  try {
+    bcrypt.compare(password, user.password);
+  } catch {
+    res.status(500).send();
+  }
+});
+//currently anyone can upload an item...
 app.post("/uploadItem", (req, res) => {
   const { image } = req.body;
 
