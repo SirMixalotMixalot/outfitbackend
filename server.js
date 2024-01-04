0fe1d1ce714e6d10338134a1eda4bf35fb8d23c0
Bokang Keith Mogomotsi <bigbk75@gmail.com>
2024-01-03 20:40:24 -0500

    Update server.js

--- server.js
+++ server.js
@@ -70,7 +70,11 @@ async function main() {
 }
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    allowedHeaders: ["x-access-token"],
+  })
+);
 app.options("*", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
