d41d682084816b88c2a2bff27f16226d3ad59aa8
Bokang Keith Mogomotsi <bigbk75@gmail.com>
2024-01-09 11:47:24 -0500

    Removed config

--- helpers/upload.js
+++ helpers/upload.js
@@ -1,11 +1,5 @@
 const cloudinary = require("cloudinary").v2;
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
 const uploadToCloudinary = async (imageBuffer) => {
   try {
     const result = await new Promise((resolve, reject) => {
